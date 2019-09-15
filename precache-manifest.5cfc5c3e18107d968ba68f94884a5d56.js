af22c5a951ca2eb9fc12d84b08d70771a6deb2f4
sliu7190 <sliu7190@uni.sydney.edu.au>
2019-09-15 19:47:06 +1000

    Updates

---
+++ precache-manifest.5cfc5c3e18107d968ba68f94884a5d56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4adfde94f57e0b5051f51f9cd89e73e",
+    "url": "/wavelet-ballot-paper/index.html"
+  },
+  {
+    "revision": "aa3d42558b60715a9e9c",
+    "url": "/wavelet-ballot-paper/static/css/main.409c6b00.chunk.css"
+  },
+  {
+    "revision": "d84ed770c4d76d00c26c",
+    "url": "/wavelet-ballot-paper/static/js/2.25bda879.chunk.js"
+  },
+  {
+    "revision": "aa3d42558b60715a9e9c",
+    "url": "/wavelet-ballot-paper/static/js/main.37da8fd6.chunk.js"
+  },
+  {
+    "revision": "8a14838e7db2ac273e87",
+    "url": "/wavelet-ballot-paper/static/js/runtime~main.51600670.js"
+  }
+]);
